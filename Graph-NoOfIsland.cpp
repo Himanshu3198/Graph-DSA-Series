089c6553b0d777358974415188267fdc54a12b47
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-03-23 13:00:41 +0530

    Update Graph-NoOfIsland.cpp

--- Graph-NoOfIsland.cpp
+++ Graph-NoOfIsland.cpp
@@ -72,4 +72,105 @@ int main(){
 		cout << ans <<'\n';
 	}
 	return 0;
-}  // } Driver Code Ends
+}
+
+
+
+
+
+
+void DFSRec(vector<int> A[], int N, int M, int i, int j)
+{
+    A[i][j] = 2;
+    
+    //Right Side
+    if(j+1 < M)
+    {
+        if(A[i][j+1] == 1)
+        {
+            DFSRec(A, N, M, i, j+1);
+        }
+    }
+    
+    //Left Side
+    if(j-1 >= 0)
+    {
+        if(A[i][j-1] == 1)
+        {
+            DFSRec(A, N, M, i, j-1);
+        }
+    }
+    
+    //Up side
+    if(i-1 >= 0)
+    {
+        if(A[i-1][j] == 1)
+        {
+            DFSRec(A, N, M, i-1, j);
+        }
+    }
+    
+    //Down side
+    if(i+1 < N)
+    {
+        if(A[i+1][j] == 1)
+        {
+            DFSRec(A, N, M, i+1, j);
+        }
+    }
+    
+    //South East
+    if(i+1 < N && j+1 < M)
+    {
+        if(A[i+1][j+1] == 1)
+        {
+            DFSRec(A, N, M, i+1, j+1);
+        }
+    }
+    
+    //South West
+    if(i+1 < N && j-1 >= 0)
+    {
+        if(A[i+1][j-1] == 1)
+        {
+            DFSRec(A, N, M, i+1, j-1);
+        }
+    }
+    
+    //North West
+    if(i-1 >= 0 && j-1 >= 0)
+    {
+        if(A[i-1][j-1] == 1)
+        {
+            DFSRec(A, N, M, i-1, j-1);
+        }
+    }
+    
+    //North East
+    if(i-1 >= 0 && j+1 < M)
+    {
+        if(A[i-1][j+1] == 1)
+        {
+            DFSRec(A, N, M, i-1, j+1);
+        }
+    }
+}
+
+int findIslands(vector<int> A[], int N, int M) 
+{
+    int count = 0;
+    
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < M; j++)
+        {
+            if(A[i][j] == 1)
+            {
+                DFSRec(A, N, M, i, j);
+                count++;
+            }
+        }
+    }
+    
+    return count;
+}
