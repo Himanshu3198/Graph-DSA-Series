1f3350621b150dea9ebb944744c25ae9c2415172
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-04-01 21:05:00 +0530

    Add files via upload

---
+++ Graph-SearchInMaze.cpp
@@ -0,0 +1,70 @@
+//  search in a maze based on flood fill graph
+// time complexity O(n^2)^4
+#include <bits/stdc++.h>
+using namespace std;
+vector<string> v;
+#define MAX 1000
+void dfs(int m[4][4], int sr, int sc, int rows, int cols, string ans, vector<vector<int>> &vis)
+{
+
+    if (sr >= rows || sr < 0 || sc >= cols || sc < 0)
+    {
+        return;
+    }
+
+    else if (m[sr][sc] == 0 or vis[sr][sc] == 1)
+    {
+        return;
+    }
+    else if (sr == rows - 1 && sc == cols - 1)
+    {
+        v.push_back(ans);
+        return;
+    }
+    vis[sr][sc] = 1;
+
+    dfs(m, sr - 1, sc, rows, cols, ans + 'U', vis);
+    dfs(m, sr + 1, sc, rows, cols, ans + 'D', vis);
+    dfs(m, sr, sc + 1, rows, cols, ans + 'R', vis);
+    dfs(m, sr, sc - 1, rows, cols, ans + 'L', vis);
+    vis[sr][sc] = 0;
+}
+
+vector<string> solve(int m[4][4], int n)
+{
+
+    string ans = "";
+    int sr = 0;
+    int sc = 0;
+    vector<vector<int>> vis(MAX,vector<int>(MAX));
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         vis[i][j] = 0;
+    //     }
+    // }
+
+    dfs(m, sr, sc, n, n, ans, vis);
+
+    sort(v.begin(), v.end());
+    return v;
+}
+int main()
+{
+    int n = 4;
+    int m[4][4] = {{1, 0, 0, 0},
+                   {1, 1, 0, 1},
+                   {1, 1, 0, 0},
+                   {0, 1, 1, 1}};
+
+    vector<string> result;
+
+    result = solve(m, 4);
+
+    for (auto it : result)
+    {
+        cout << it << " ";
+    }
+}
