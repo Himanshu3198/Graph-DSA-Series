e7d9c6f45a8a35b2bd3107bf967a3e923da02923
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-04-03 15:34:29 +0530

    Update Graph-MinimumStepByKnight.cpp

--- Graph-MinimumStepByKnight.cpp
+++ Graph-MinimumStepByKnight.cpp
@@ -1,48 +1,83 @@
+// Steps by Knight
+// Expected Time Complexity: O(N2).
+// Expected Auxiliary Space: O(N2).
+
+
+/* Author 
+     himanshu3198
+ */
 #include<bits/stdc++.h>
-#define N 6
 using namespace std;
+class Solution {
+public:
 
-int Knight(int chess[N][N],vector<int>&KnightPos,vector<int>&targetPos,int n){
+     
+    bool valid(int x, int y, int n){
+    if(x>=0 && x<n && y>=0 && y<n)return true;
+    else return false;
+    }
+	int minStepToReachTarget(vector<int>&KnightPos, vector<int>&TargetPos, int N){
+	    // Code here
+	    
+	      int x1=KnightPos[0];
+	      int y1=KnightPos[1];
+	      
+	      int x2=TargetPos[0];
+	      int y2=TargetPos[1];
+	    
+          int xcord[] = {2,1,-1,-2};
+    int ycord[] = {1,2,2,1};
+    
+    vector< vector<int> > board(N, vector<int>(N, -1) );
+    queue< pair<int,int> >  q;
+    
+    q.push(make_pair(x1-1,y1-1));
+    board[x1-1][y1-1] ++;
+    while(!q.empty()){
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        
+        if(x==(x2-1) && y == (y2-1) ){
+            return board[x][y];
+        }
+        
+        for(int i=0; i<4; i++){
+            int xnew = x + xcord[i];
+            int ynew = y + ycord[i];
+            if(valid(xnew, ynew, N) && board[xnew][ynew]==-1){
+                board[xnew][ynew] = board[x][y] + 1;
+                q.push(make_pair(xnew, ynew));
+            }
+            
+            ynew = y - ycord[i];
+            if(valid(xnew, ynew,N) && board[xnew][ynew]==-1){
+                board[xnew][ynew] = board[x][y] + 1;
+                q.push(make_pair(xnew, ynew));
+            }
+        }
+        
+    }
+    return -1;
+	    
+	    
+	}
+};
 
- int x1=KnightPos[0];
- int y1=KnightPos[1];
- int x2=targetPos[0];
- int y2=targetPos[1];
-
- if(x1==x2 and y1==y2){
-     return 0;
- }
-
- queue<pair<int,int>>q;
- q.push(make_pair(x1-1,y1-1));
-
- while(!q.empty()){
-
-     auto cur=q.front();
-     int i=cur.first;
-     int j=cur.second;
-     q.pop();
-
-
-     if((i+1)>=0 and (i+1)<n and (j+2)>=0 and (j+2)<n and )
-
- }
-
-   
-
-}
 
 int main(){
-
-    int chess[N][N];
-    vector<int>knightPos={4,5};
-    vector<int>targetPos={1,1};
-
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
-            chess[i][j]=0;
-        }
-    }
-
-    cout<<Knight(chess,knightPos,targetPos,N);
-}
+	int tc;
+	cin >> tc;
+	while(tc--){
+		vector<int>KnightPos(2);
+		vector<int>TargetPos(2);
+		int N;
+		cin >> N;
+		cin >> KnightPos[0] >> KnightPos[1];
+		cin >> TargetPos[0] >> TargetPos[1];
+		Solution obj;
+		int ans = obj.minStepToReachTarget(KnightPos, TargetPos, N);
+		cout << ans <<"\n";
+	}
+	return 0;
+}  
