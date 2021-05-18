e41297da5575d2816413bab2a27e2ba6a4e9c1aa
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-05-18 20:42:11 +0530

    Create graph-detect-cycle-in-undirected-graph.cpp

---
+++ graph-detect-cycle-in-undirected-graph.cpp
@@ -0,0 +1,49 @@
+// detect cycle in undirected graph    
+bool isCyclic(vector<int>adj[],vector<int>visit,int curr,int V){
+        
+        if(visit[curr]==2){
+            return true;
+        }
+        
+        visit[curr]=1;
+        
+        for(auto it:adj[curr]){
+            
+            if(visit[it]==1){
+                
+                visit[it]=2;
+                
+            }
+            else{
+                
+                if(isCyclic(adj,visit,it,V)){
+                    
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+	bool isCycle(int V, vector<int>adj[])
+	{
+	    // Code here
+	    
+	    
+	    vector<int>visit(V,0);
+	    
+	    
+	    for(int i=0;i<V;i++){
+	          visit[i]=1;
+	        for(auto it:adj[i]){
+	            
+	            if(isCyclic(adj,visit,it,V)){
+	                return true;
+	            }
+	        }
+	        
+	        visit[i]=0;
+	    }
+	    
+	    return false;
+	}
