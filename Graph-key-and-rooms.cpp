8421665b5e6c538b7977acbb8432aeeb005bf1d5
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-07-15 16:40:02 +0530

    Update Graph-key-and-rooms.cpp

--- Graph-key-and-rooms.cpp
+++ Graph-key-and-rooms.cpp
@@ -3,15 +3,17 @@
 class Solution {
 public:
     
+    
+      /*   the problem statement say that we have n rooms labeled as 0 to n-1  and we have to visit the all 
+            the rooms. for visting the room we need a key  to unlock that room and we found key of others room in      
+             current room  so basically we can visit a room only if have  a key this statement is repersent as graph 
+             and its adjacent vertices represented in the form of key do simple dfs of this graph and later check if we have
+            successfully visited the  all rooms or  not if yes then return true else false.
+            */
 void dfs(vector<vector<int>>& rooms,vector<int>&visit,int node){
         
       
-        /*   the problem statement say that we have n rooms numbered as 0 t0 n-1  and we have visit the all 
-             rooms for visting the room we need key to unlock the room and we found key of others room in      
-             current room  so basically we can visit a room if have key this statement is repersent as graph 
-             and its adjacent vertices in the form of key do simple dfs and later check if we have
-             visited all rooms or  not if yes then return true else false.
-            */
+      
         
         visit[node]=1;
         
