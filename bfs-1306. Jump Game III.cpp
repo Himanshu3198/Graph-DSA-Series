74797140be6d479872a12429c56b41e75a852b74
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-12-10 00:02:49 +0530

    Update bfs-1306. Jump Game III.cpp

--- bfs-1306. Jump Game III.cpp
+++ bfs-1306. Jump Game III.cpp
@@ -1,3 +1,33 @@
+//  dfs 
+//  time & space complexity O(n)
+class Solution {
+public:
+    
+     bool solve(vector<int>&arr,int idx,vector<int>&vis){
+         
+         
+         if(idx<0 or idx>=arr.size() ) return false;
+         
+          if(vis[idx]==1){
+              return false;
+          }
+         if(arr[idx]==0) return true;
+         
+         
+            vis[idx]=1;
+         
+         return solve(arr,idx+arr[idx],vis) || solve(arr,idx-arr[idx],vis);
+     }
+    bool canReach(vector<int>& arr, int start) {
+    
+           vector<int>vis(arr.size(),0);
+        return solve(arr,start,vis);
+     
+
+    }
+};
+// bfs approach  
+time & space comeplexity: O(n) 
 class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
