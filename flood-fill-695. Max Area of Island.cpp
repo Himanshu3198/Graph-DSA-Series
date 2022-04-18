61271f288fd85db91779a9047af930d56a83abd8
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-04-18 20:07:42 +0530

    Update flood-fill-695. Max Area of Island.cpp

--- flood-fill-695. Max Area of Island.cpp
+++ flood-fill-695. Max Area of Island.cpp
@@ -1,45 +1,33 @@
-// 695. Max Area of Island
-
 class Solution {
 public:
     
-    
-       int dfs(vector<vector<int>>& grid,vector<vector<bool>>&vis , int i,int j,int n,int m){
-           
-           
-           if(i<0 or i>=n or j<0 or j>=m or vis[i][j] or !grid[i][j] ){
-               return 0;
-           }
-           
-           vis[i][j]=true;
-           
-           return 1+dfs(grid,vis,i+1,j,n,m)+
-                   dfs(grid,vis,i-1,j,n,m)+
-               dfs(grid,vis,i,j+1,n,m)+
-               dfs(grid,vis,i,j-1,n,m);
-       }
+     int dx[4]={-1,1,0,0};
+     int dy[4]={0,0,-1,1};
+    bool isValid(vector<vector<int>>&grid,int i,int j,int n,int m){
+        if(i>=0 and i<n and j>=0 and j<m and grid[i][j]==1) return true;
+       return false;
+    }
+    int dfs(vector<vector<int>>& grid,int i,int j,int n,int m){
+        if(!isValid(grid,i,j,n,m)) return 0;
+         grid[i][j]=0;
+         int ans=0;
+         for(int k=0;k<=3;k++){
+             int x=dx[k]+i;
+             int y=dy[k]+j;
+             ans+=dfs(grid,x,y,n,m);
+         }
+         return ans+1;
+    }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        
-        
-        int maxArea=0;
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<bool>>vis(n,vector<bool>(m,false));
-        
-        
+        int maxArea=0;
         for(int i=0;i<n;i++){
-            
             for(int j=0;j<m;j++){
-                
-                if(!vis[i][j] and grid[i][j]){
-                    
-                    int curArea=dfs(grid,vis,i,j,n,m);
-                    
-                    maxArea=max(maxArea,curArea);
-                }
+                if(grid[i][j]==1)
+                    maxArea=max(maxArea,dfs(grid,i,j,n,m));    
             }
         }
-        
-        return maxArea;
+        return maxArea;  
     }
 };
