1ea06438ac9a8d1258a5449b188e3025619aa72c
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-04-26 15:47:22 +0530

    Update graph-detect-cycle-in-undirected-graph.cpp

--- graph-detect-cycle-in-undirected-graph.cpp
+++ graph-detect-cycle-in-undirected-graph.cpp
@@ -1,3 +1,37 @@
+
+//  method 1  dsu 
+    int findPar(int x,vector<int>&parent){
+          if(parent[x]==x) return x;
+          return parent[x]=findPar(parent[x],parent);
+      }
+      void union_op(int x,int y,vector<int>&parent,vector<int>&rank){
+           x=findPar(x,parent);
+           y=findPar(y,parent);
+           if(x==y) return;
+           if(x>y){
+               parent[y]=x;
+               rank[x]+=1;
+           }else{
+               parent[x]=y;
+               rank[y]+=1;
+           }
+      }
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+           vector<int>rank(V,0),parent(V);
+           vector<int>vis(V,0);
+           for(int i=0;i<V;i++) parent[i]=i;
+           for(int i=0;i<V;i++){
+               vis[i]++;
+               for(auto &it:adj[i]){
+                   if(vis[it]) continue;
+                   if(findPar(it,parent)==findPar(i,parent)) return true;
+                   else union_op(i,it,parent,rank);
+               }
+           }
+           return false;
+    }
+
 // detect cycle in undirected graph    
 bool isCyclic(vector<int>adj[],vector<int>visit,int curr,int V){
         
