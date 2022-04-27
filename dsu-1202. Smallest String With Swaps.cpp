005d3e19c7d536da108f4704175ec37b2d9b65e7
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-04-27 15:24:57 +0530

    Update dsu-1202. Smallest String With Swaps.cpp

--- dsu-1202. Smallest String With Swaps.cpp
+++ dsu-1202. Smallest String With Swaps.cpp
@@ -57,3 +57,55 @@ public:
         return s;
     }
 };
+
+class Solution {
+public:
+    
+    int findPar(int x,vector<int>&parent){
+        if(parent[x]==x) return x;
+        return parent[x]=findPar(parent[x],parent);
+    }
+    void union_op(int u,int v,vector<int>&parent,vector<int>&rank){
+        int x=findPar(u,parent);
+        int y=findPar(v,parent);
+        if(x==y) return;
+        if(rank[x]>rank[y]){
+            parent[y]=x;
+            rank[x]+=1;
+        }else{
+            parent[x]=y;
+            rank[y]+=1;
+        }
+    }
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+            int n=s.size();
+            vector<int>rank(n,0);
+            vector<int>parent(n);
+            for(int i=0;i<n;i++) parent[i]=i;
+            for(int i=0;i<pairs.size();i++){
+                int u=pairs[i][0];
+                int v=pairs[i][1];
+                union_op(u,v,parent,rank); 
+            }
+            unordered_map<int,vector<int>>mp;
+            for(int i=0;i<n;i++){
+                 mp[findPar(i,parent)].push_back(i);
+            }
+             
+           for(auto it:mp){
+            
+               vector<int>index;
+               vector<char>letters;
+               for(auto v:it.second){
+                   letters.push_back(s[v]);
+                   index.push_back(v);
+               }
+               sort(index.begin(),index.end());
+               sort(letters.begin(),letters.end());
+                 for(int i=0;i<index.size();i++){
+                     s[index[i]]=letters[i];
+                 }
+           }
+          return s;
+    }
+};
