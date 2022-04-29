5d22e05b69f4c587103647800edb4af6bcbc777a
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-04-30 00:15:00 +0530

    Update Graph-check-Bipartite.cpp

--- Graph-check-Bipartite.cpp
+++ Graph-check-Bipartite.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     
+   /* 
+     A bipartite is a graph which can be divded into two different half
+     to check given graph is bipartite we will mark vertex with 2 colors
+     if there is no adjacent has same color then graph is bipartite otherwise
+     no.
+      time complexity O(E+V
+      Space complexity O(V)
+      */
+    
      vector<int>color;
-    bool dfs(int node,int parentColor,vector<vector<int>>& graph){
+    bool dfs(int node,int &parentColor,vector<vector<int>>& graph){
         
         if(color[node]!=0){
             return color[node]!=parentColor;
@@ -16,10 +25,11 @@ public:
     bool isBipartite(vector<vector<int>>& graph) {
         
         int n=graph.size();
-        color.assign(n+1,0);
+        color.assign(n,0);
+        int parentColor=-1;
         for(int i=0;i<n;i++){
             
-            if(!dfs(i,-1,graph)) return false;
+            if(color[i]==0 and !dfs(i,parentColor,graph)) return false;
         }
         return true;
         
