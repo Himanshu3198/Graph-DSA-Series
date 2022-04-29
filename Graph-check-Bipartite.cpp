002c2ee8ca72020856efc10eba224524c3138bfc
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-04-29 23:53:59 +0530

    Update Graph-check-Bipartite.cpp

--- Graph-check-Bipartite.cpp
+++ Graph-check-Bipartite.cpp
@@ -1,69 +1,28 @@
-
- // } Driver Code Ends
 class Solution {
 public:
-	bool isBipartite(int V, vector<int>adj[]){
-	    // Code here
     
+     vector<int>color;
+    bool dfs(int node,int parentColor,vector<vector<int>>& graph){
         
-        vector<int>visit(V,0);
-        
-        queue<int>q;
-        for(int i=0;i<V;i++){
-            
-            if(visit[i]==1 || visit[i]==-1){
-                continue;
-            }
-            
-            visit[i]=1;
-            q.push(i);
-        
-        
-        while(!q.empty()){
-            
-            int first=q.front();
-            q.pop();
-            
-            for(int j:adj[first]){
-                
-               if(visit[j]==0){
-                   visit[j]=visit[first]*-1;
-                   // visit[j]=-1;
-                   q.push(j);
-               }
-                if(visit[first]==visit[j]){
-                    return false;
-                }
-            }
+        if(color[node]!=0){
+            return color[node]!=parentColor;
         }
-     
+        color[node]=-parentColor;
+        for(auto it:graph[node]){
+            if(!dfs(it,color[node],graph)) return false;
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        
+        int n=graph.size();
+        color.assign(n+1,0);
+        for(int i=0;i<n;i++){
+            
+            if(!dfs(i,-1,graph)) return false;
+        }
+        return true;
+        
         
     }
-           return true;
-	    
-	    
-	}
-
 };
-
-// { Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int V, E;
-		cin >> V >> E;
-		vector<int>adj[V];
-		for(int i = 0; i < E; i++){
-			int u, v;
-			cin >> u >> v;
-			adj[u].push_back(v);
-			adj[v].push_back(u);
-		}
-		Solution obj;
-		bool ans = obj.isBipartite(V, adj);    
-		if(ans)cout << "1\n";
-		else cout << "0\n";  
-	}
-	return 0;
-}  // } Driver Code Ends
