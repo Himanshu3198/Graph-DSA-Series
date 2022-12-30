53b5b5bb2ebc68942337963302a540fdc2cd8dca
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-12-30 22:25:29 +0530

    Update graph-all-path-from-src-target.cpp

--- graph-all-path-from-src-target.cpp
+++ graph-all-path-from-src-target.cpp
@@ -3,44 +3,52 @@
 
 class Solution {
 public:
-    
-    
-    
-    void dfs(vector<vector<int>>&graph,vector<vector<int>>&res,vector<int>v,int src,int dst){
+    vector<vector<int>>res;
+    void dfs(unordered_map<int,vector<int>>&graph,vector<int>temp,int src,int dest){
         
-         v.push_back(src);
-        
-        
-          if(src==dst){
-              
-              res.push_back(v);
-              return;
-          }
-        
-           
-          for(auto it:graph[src]){
-              
-              dfs(graph,res,v,it,dst);
-          }
-        
-        
+        if(src==dest){
+            temp.push_back(src);
+            res.push_back(temp);
+            return;
+        }
+        temp.push_back(src);
+        for(auto &it:graph[src]){
+             dfs(graph,temp,it,dest);
+        }
+        temp.pop_back();
     }
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-     
-           vector<vector<int>>res;
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>&g) {
         
-          int n=graph.size();
-        
-              if(n==0){
-                  return res;
-              }
-        
-         int src=0,dst=n-1;
-        
-        vector<int>v;
-        
-        dfs(graph,res,v,src,dst);
-        
+        unordered_map<int,vector<int>>graph;
+        int n=g.size()-1;
+        for(int i=0;i<=n;i++){
+            vector<int>curr=g[i];
+            graph[i]=curr;
+        }
+        vector<int>temp;
+        dfs(graph,temp,0,n);
         return res;
     }
 };
+
+class Solution {
+    public void dfs(int[][]graph,List<Integer>temp,List<List<Integer>>res,int src,int dest){
+        if(src==dest){
+            res.add(new ArrayList<Integer>(temp));
+            return;
+        }
+        for(int it:graph[src]){
+            temp.add(it);
+            dfs(graph,temp,res,it,dest);
+            temp.remove(temp.size()-1);
+        }
+    }
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>>res=new ArrayList<>();
+        List<Integer>temp=new ArrayList<>();
+        int n=graph.length;
+        temp.add(0);
+        dfs(graph,temp,res,0,n-1);
+        return res;
+    }
+}
