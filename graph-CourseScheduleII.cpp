970c57caed5fcd4bc68b8ff099051f74c88b2bcd
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2025-12-07 02:49:44 +0530

    Add Course Schedule II solution in Java
    Implement a solution for Course Schedule II using graph traversal.

--- graph-CourseScheduleII.cpp
+++ graph-CourseScheduleII.cpp
@@ -41,3 +41,45 @@ public:
          
     }
 };
+// java code 
+
+class Solution {
+    public int[] findOrder(int num, int[][] pre) {
+        
+          
+          Map<Integer,List<Integer>> graph = new HashMap<>();
+          int [] indegree = new int[num];
+          for(int [] p:pre){
+
+               int a = p[0];
+               int b = p[1];
+               graph.putIfAbsent(b,new ArrayList<>());
+               graph.get(b).add(a);
+               indegree[a]++;
+          }
+          
+          Queue<Integer> q = new LinkedList<>();
+          for(int i=0;i<num;i++){
+            if(indegree[i] == 0) q.offer(i);
+          }
+
+          int [] result = new int[num];
+          int idx = 0;
+
+          while(!q.isEmpty()){
+
+            int course = q.poll();
+            result[idx++] = course;
+            for( var ngbr: graph.getOrDefault(course,new ArrayList<>())){
+                 indegree[ngbr] = indegree[ngbr] -1;
+                 if(indegree[ngbr] == 0){
+                    q.offer(ngbr);
+                 }
+            }
+          }
+          
+          int [] invalid = new int[num];
+          if(idx != num) return new int[0];
+          return result;
+    }
+}
